4ec95c60382cf29b7d42032b47007ade8b80c446
Unchpokable <Unchpokable.official@gmail.com>
2024-11-19 16:14:13 +0500

    empty definitions of Injector functions

--- NecromancyLoader/injector.cpp
+++ NecromancyLoader/injector.cpp
@@ -1,2 +1,38 @@
 #include "pch.h"
 #include "injector.h"
+
+bool WinDllInjector::setTargetLibrary(WinConstStr libraryName) {
+    return false;
+}
+
+bool WinDllInjector::setTargetProc(WinConstStr procName) {
+    return false;
+}
+
+bool WinDllInjector::setTargetProcPid(WinDword procId) {
+    return false;
+}
+
+WinResult WinDllInjector::inject() {
+    return WinResult();
+}
+
+WinResult WinDllInjector::free() {
+    return WinResult();
+}
+
+bool WinDllInjector::hasTargetLoadedLibrary() {
+    return false;
+}
+
+WinResult WinDllInjector::targetFreeLibrary() {
+    return WinResult();
+}
+
+bool WinDllInjector::isTargetLibraryX86() {
+    return false;
+}
+
+bool WinDllInjector::isTargetProcX86() {
+    return false;
+}
