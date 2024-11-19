2b1611e523cdec4e7cfe76c4fa0cd1b35309942f
Unchpokable <Unchpokable.official@gmail.com>
2024-11-20 02:21:47 +0500

    ui for loader

--- NecromancyLoader/necromancyloaderwindow.cpp
+++ NecromancyLoader/necromancyloaderwindow.cpp
@@ -5,6 +5,7 @@ NecromancyLoaderWindow::NecromancyLoaderWindow(QWidget *parent)
         : QMainWindow(parent)
         , ui(new Ui::NecromancyLoaderWindowClass()) {
     ui->setupUi(this);
+    setWindowTitle("Necromancy Loader");
 }
 
 NecromancyLoaderWindow::~NecromancyLoaderWindow() {
